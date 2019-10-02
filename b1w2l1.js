7a7d7da045a515c91871888b4cd15971aa420121
simon <doulissimon@gmail.com>
2019-10-02 09:23:37 +0200

    game
    game

---
+++ b1w2l1.js
@@ -0,0 +1,94 @@
+window.alert("welcome to the game");
+window.alert("je bent een persoon en je antwoord met 69,420 of 666");
+naam = window.prompt("wat is je naam?")
+window.alert(naam + "? ,wauw wat een kut naam")
+window.alert("GA VERDER OM DE GAME TE BEGINNEN")
+window.alert("je staat voor de KFC in dordrecht, de heerlijke vettige KFC lucht hangt in de lucht. Je krijgt er honger van. wat doe je")
+var route1 = window.prompt("69 = je gaat in de rij staan bij de KFC, 420 = je gaat bij een willekeurig persoon aan tafel zitten, 666 = je besluit niet naar de kfc te gaan en draait je om")
+if (route1 == "666") {
+	window.alert("je staat nu voor de kfc en er zijn meerdere keuzes die je kunt maken")
+var route1r1 = window.prompt("69  = praat met de zwerver naast je 420 = loop terug in de kfc")
+}
+if (route1r1 == "69") {
+	window.alert("de zwerver verteld je zijn levensverhaal en hij praat je om om ook een zwerver te worden. je verkoopt je huis en koopt er vbucks van. YOU GOT THE ZWERVER ENDING")
+}
+if (route1r1 == "420") {
+	window.alert("terwijl je terug loopt in de kfc struikel je over een kfc bucket tot je dood. KFCDOODENDING") 
+}
+if (route1 == "420") {
+	var route1r2 = window.prompt("je kunt bij 2 personen aan tafel zitten 69 = een jongeman die boos naar een kfc bucket kijkt 420 = een man met een blauwe bloes en een man met een lange baard")
+}
+if (route1r2 == "69") {
+	window.alert(naam + ": hallo goedendag jongeman wat is uw naam?") 
+	window.alert("martijn: martijn kooiman is de naam")
+	window.alert(naam + ": waarmom kijkt je zo boos naar je kfc bucket?")
+    window.alert("martijn: Ik werk bij de mac, dat is de concurrent")
+    window.alert("martijn: welke vind jij beter kfc of mac?")
+    var route1r2r1 = window.prompt("69 = kfc 420 = mac")
+}
+if (route1r2r1 == "69") {
+	window.alert("martijn: WAAAAATTTTT????")
+	window.alert("je wordt wakker op een trein spoor, vastgebonden")
+	window.alert("terwijl je wakker wordt hoor je langzaam een trein geluid steeds dichterbij komen")
+	window.alert("TJOEK TJOEK een trein komt in jouw richting met daarin een autist die kwaadaardig lacht")
+	window.alert("you got hit by train. je kreeg de TREINENDOODENDING")
+}
+if (route1r2r1 == "420") {
+	window.alert("martijn: lekker man")
+	window.alert("je wordt wakker in de macdonalds keuken op de grond")
+	window.alert("je kijkt naar beneden en je draagt een macdonalds pak")
+	window.alert("martijn: hallo " + naam + ", nieuwe collega :D") 
+	window.alert("you got MACDONALDS ENDING")
+}
+if (route1r2 == "420") {
+	window.alert("je gaat bij de tafel zitten")
+	window.alert(naam + ": goedendag mannen hoe heten jullie?")
+	window.alert("rudi: ik ben rudi en ik heb een mooi baardje")
+	window.alert("bouman: ik ben bouman en ik ben een applicatie ontwikkelaar leraar op het davinci in gorinchem")
+	window.alert(naam + ": aangenaamd kennis te maken")
+	var route1r3 = window.prompt("69 = maak een beledeging over rudi zijn baardje 420 = complimenteer rudi zijn baardje 666 = eet meneer bouman zijn kfc bucket onverwachts op")
+}
+if (route1r3 == "69") {
+	window.alert(naam + ": rudi je baard lijkt op een ontplofte egel")
+	window.alert("rudi: >:(")
+	window.alert("je wordt wakker vastgebonden op de grond. Met om je heen 10 katten in een perfect gelegde cirkel")
+	window.alert("je hoort een zacht gefluister steeds harder worden")
+	window.alert("luci, rover, fikkie, jadis, tabby, yuki, viva, chase, jinx, smokey")
+	window.alert("LUCI ROVER FIKKIE JADIS TABBY...")
+	window.alert("je realizeert je nu dat je in een satanisch ritueel wordt opgeofferd")
+	window.alert("je lichaam verbrand in as terwijl rudi op de achtergrond kwaadaardig")
+	window.alert("you got the OPOFFEREINDE")
+}
+if (route1r3 == "420") {
+	window.alert(naam + ": hey rudi mooi baardje :D")
+	window.alert("rudi: bedankt :3")
+	window.alert("hierna ging je samen met rudi en bouman kfc eten you got HAPPYENDING")
+}
+if (route1r3 == "666") {
+	window.alert("je eet bouman zijn kfc bucket op")
+	window.alert(naam + ": wauw dat was verukkelijk")
+	window.alert("bouman: tyfus jong dat was mijn kfc bucket!")
+	window.alert("je besteld een nieuwe bucket en die eet je dan samen op met rudi en bouman")
+	window.alert("you got the GEZELLIGELERARENENDING")
+}
+if (route1 == "69") {
+	window.alert("je staat nu in de rij bij de kfc")
+	window.alert("er staat een groot bord met 2 etenswaren kip of chinees")
+	var route1r4 = window.prompt("welke kies je 69 = kip 420 = chinees")
+}
+if (route1r4 == "69") {
+	window.alert("wat voor kip?")
+	var route1r4r1 = window.prompt("69 = boneless bucket 420 = family bucket")
+}
+if (route1r4r1 == "69") {
+	window.alert("je had een lekkere maaltijd bij de kfc GOEDEINDE")
+}
+if (route1r4r1 == "420") {
+	window.alert("je eet het allemaal op, maar omdat het ene family bucket is is het iets teveel")
+	window.alert("je krijgt diabetes van de kip OOFEINDE")	
+}
+if (route1r4 == "420") {
+	window.alert("er is geen chinees bij de kfc, dat is illegaal")
+	window.alert("je wordt gearresteerd door de kfc bewakers en in de gevangenis gezet")
+	window.alert("you got GEVANGENISEINDE")
+}
